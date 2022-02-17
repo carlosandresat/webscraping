a1694e192eb28be3189dcbee6a98a2c4fa831ee3
carlosandresat <carlosandresat@hotmail.com>
2022-02-17 14:48:02 -0500

    data formatting from scraper

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { cp } = require('fs');
 
 const url = "https://news.ycombinator.com/";
 
 const getContent = $ => 
     $('.athing').map((_, a) => {
         const $a = $(a);
+        var order = $a.find('.rank').text();
         var id_score = "#score_" + $a.attr('id');
-        const score = $(id_score).text();
-        const commentsNo = $(id_score).parent().children("a").last().text();
+        var score = $(id_score).text();
+        var commentsNo = $(id_score).parent().children("a").last().text();
 
-        return{
-            title: $a.find('.titlelink').text(),
-            order: $a.find('.rank').text(),
-            comments: commentsNo,
-            score: score,
-        };
+        order = order.match((/(\d+)/))
+        commentsNo = commentsNo.match((/(\d+)/));
+        score = score.match((/(\d+)/));
+
+        if (commentsNo != null && score != null){
+            return{
+                title: $a.find('.titlelink').text(),
+                order: parseInt(order[0]),
+                comments: parseInt(commentsNo[0]),
+                score: parseInt(score[0]),
+            };
+        }
+        else if (commentsNo == null && score != null){
+            return{
+                title: $a.find('.titlelink').text(),
+                order: parseInt(order[0]),
+                comments: 0,
+                score: parseInt(score[0]),
+            };
+        }
+        else if (commentsNo != null && score == null){
+            return{
+                title: $a.find('.titlelink').text(),
+                order: parseInt(order[0]),
+                comments: parseInt(commentsNo[0]),
+                score: 0,
+            };
+        }
+        else{
+            return{
+                title: $a.find('.titlelink').text(),
+                order: parseInt(order[0]),
+                comments: 0,
+                score: 0,
+            };
+        }
     }).toArray();
 
 const init = async () => {
